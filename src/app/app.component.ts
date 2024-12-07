782b32b336851da6562dc5ef0ce3eeea48f05177
Marisha <marinasemenovaa1@gmail.com>
2024-12-07 20:34:32 +0100

    add decripton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import {HeaderComponent} from './header/header.component';
 import {TaskListComponent} from './task-list/task-list.component';
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,
+    FormsModule,
     HeaderComponent,
     TaskListComponent],
   templateUrl: './app.component.html',
--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,18 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.css'
 })
 export class TaskListComponent {
+
 tasks = [
-   { name: 'Shopping', done: false },
-   { name: 'Training', done: false },
-   { name: 'Codding', done: false }
+   { name: 'Shopping', description:'buy something', done: false },
+   { name: 'Training', description:'', done: false },
+   { name: 'Codding', description:'', done: false }
   ];
  toggleTaskStatus(index: number): void {
     this.tasks[index].done = !this.tasks[index].done;
